8dd489a08d12a09ab5a265e0d56c90bb3f7221fa
David Boone <Dboone2@mail.greenriver.edu>
2021-04-15 12:30:32 -0400

    Added index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+    /*
+     * Author: David Boone
+     * Date: 04 - 15 - 21
+     * File: index.php
+     * Pair Program 2
+     */
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pair Program 2</title>
+</head>
+<body>
+    <header>
+        <h1>Pair Program 2</h1>
+    </header>
+    <?php
+        echo "<h1>PHP Array Practice</h1>";
+    ?>
+</body>
+</html>
